5ecdf25ec223c502b2489f8406cd8eb06f7050ba
Kostiantyn Aleksieienko <00gevill00@gmail.com>
2020-09-02 20:11:03 +0300

    Update visitor

--- src/main/java/com/aleksieienko/brainfuck/compiler/visitor/Visitor.java
+++ src/main/java/com/aleksieienko/brainfuck/compiler/visitor/Visitor.java
@@ -1,18 +1,22 @@
 package com.aleksieienko.brainfuck.compiler.visitor;
 
-import com.aleksieienko.brainfuck.compiler.commands.CycleCommand;
-import com.aleksieienko.brainfuck.compiler.commands.DecrementCommand;
-import com.aleksieienko.brainfuck.compiler.commands.IncrementCommand;
-import com.aleksieienko.brainfuck.compiler.commands.NextCellCommand;
-import com.aleksieienko.brainfuck.compiler.commands.PrevCellCommand;
-import com.aleksieienko.brainfuck.compiler.commands.PrintCommand;
-import java.io.IOException;
+import com.aleksieienko.brainfuck.compiler.commands.impl.CycleCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.DecrementCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.EndCycleCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.IncrementCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.NextCellCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.PrevCellCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.PrintCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.StartCycleCommand;
 
 public interface Visitor {
-    Object visitCycleCommand(CycleCommand cycleCommand) throws IOException;
+
     Object visitDecrementCommand(DecrementCommand decrementCommand);
     Object visitIncrementCommand(IncrementCommand incrementCommand);
     Object visitNextCellCommand(NextCellCommand nextCellСommand);
     Object visitPrevCellCommand(PrevCellCommand prevCellCommand);
     Object visitPrintCommand(PrintCommand printCommand);
+    Object visitEndCycleCommand(EndCycleCommand endCycleCommand);
+    Object visitStartCycleCommand(StartCycleCommand startCycleCommand);
+    Object visitCycleCommand(CycleCommand cycleCommand);
 }
--- src/main/java/com/aleksieienko/brainfuck/compiler/visitor/impl/ReadVisitor.java
+++ src/main/java/com/aleksieienko/brainfuck/compiler/visitor/impl/ReadVisitor.java
@@ -1,40 +1,28 @@
 package com.aleksieienko.brainfuck.compiler.visitor.impl;
 
 import com.aleksieienko.brainfuck.compiler.commands.Command;
-import com.aleksieienko.brainfuck.compiler.commands.CycleCommand;
-import com.aleksieienko.brainfuck.compiler.commands.DecrementCommand;
-import com.aleksieienko.brainfuck.compiler.commands.IncrementCommand;
-import com.aleksieienko.brainfuck.compiler.commands.NextCellCommand;
-import com.aleksieienko.brainfuck.compiler.commands.PrevCellCommand;
-import com.aleksieienko.brainfuck.compiler.commands.PrintCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.CycleCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.DecrementCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.EndCycleCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.IncrementCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.NextCellCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.PrevCellCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.PrintCommand;
+import com.aleksieienko.brainfuck.compiler.commands.impl.StartCycleCommand;
 import com.aleksieienko.brainfuck.compiler.visitor.Visitor;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.LinkedList;
+import java.util.Deque;
+import java.util.List;
 
 public class ReadVisitor implements Visitor {
-    private Reader in;
+    private Deque<Command> stack;
 
-    public ReadVisitor(Reader in){
-        this.in = in;
-    }
-
-    @Override
-    public Command visitCycleCommand(CycleCommand cycleCommand) throws IOException {
-        CycleCommand newCycleCommand = new CycleCommand();
-        for(int chr; ((chr = in.read()) != ']');) {
-            if(Command.commandMap.get((char)chr) == null) {
-                return null;
-            } else {
-                Command command = (Command) Command.commandMap.get((char) chr).accept(this);
-                if(command == null){
-                    return null;
-                }
-                newCycleCommand.add(command);
-            }
+    public ReadVisitor(List<Command> stack) throws Exception {
+        if(!(stack instanceof Deque)){
+            throw new Exception("Incorrect type of list");
         }
-        return newCycleCommand;
+        this.stack = (Deque<Command>) stack;
     }
+
 
     @Override
     public Command visitDecrementCommand(DecrementCommand decrementCommand) {
@@ -57,7 +45,30 @@ public class ReadVisitor implements Visitor {
     }
 
     @Override
+    public Object visitStartCycleCommand(StartCycleCommand startCycleCommand) {
+        return startCycleCommand;
+    }
+
+    @Override
+    public Command visitEndCycleCommand(EndCycleCommand endCycleCommand) {
+        CycleCommand cycleCommand = new CycleCommand();
+        Command command;
+
+        while(!(command = stack.pollLast()).equals(new StartCycleCommand())
+                && command != null) {
+            cycleCommand.addFirst(command);
+        }
+
+        return cycleCommand;
+    }
+
+    @Override
     public Command visitPrintCommand(PrintCommand printCommand) {
         return printCommand;
+    }
+
+    @Override
+    public Object visitCycleCommand(CycleCommand cycleCommand) {
+        return cycleCommand;
     }
 }
